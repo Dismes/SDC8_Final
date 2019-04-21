bb84d5b0d54310206367e4fb7b9121ea6dbcea07
Yuki Nakano <yuhki.n@gmail.com>
2019-04-21 14:29:05 -0700

    work in progress

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,5 @@
 
     $('.toptext').on('click', (evt) => {
-        $(this).hide(); // does not run a DOM query
-        //$('.class-name').hide() // runs a DOM query
-        var test = $(this)
-        console.log($(this).attr('id'));
+        console.log(document.getElementById(this));
+        console.log('test');
     }); 
-  
